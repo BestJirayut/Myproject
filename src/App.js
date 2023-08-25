839b839d850ccc180a314fb489431b2ad92768dc
BestJirayut <jpb.jirayut@gmail.com>
2023-08-25 14:09:10 +0700

    first profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      
+        <section>
+          <div class="container">
+            <div class="background-img">
+              <div class="box">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <div class="content">
+                  <img src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/316426651_1858924314447099_728442041240213632_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=WbRJWp5XPkoAX9J3hFZ&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfAsCfcKCte8cy1Vtr4oka4i0pFQ_ctK3e0E4SRtmgmmSg&oe=64EE3A6F" alt="" width="280"></img>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <th>ชื่อ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                      <tr>
+                        <th>อายุ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                      <tr>
+                        <th>ชื่อ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                      <tr>
+                        <th>ชื่อ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                      <tr>
+                        <th>ชื่อ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                      <tr>
+                        <th>ชื่อ</th>
+                        <td>จิรายุทธ ภูมิชัย</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+        
+              </div>
+            </div>
+          </div>
+        </section>
     </div>
   );
 }
